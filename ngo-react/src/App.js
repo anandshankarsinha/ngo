55e16b8cabbd3d8ced49a4109dff9f9887b7ae13
Anand Shankar <110841170+anandshankarsinha@users.noreply.github.com>
2023-11-09 19:03:46 +0530

    footer and home page succesfully completed

--- ngo-react/src/App.js
+++ ngo-react/src/App.js
@@ -3,12 +3,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Body from "./Components/Body";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <RouterProvider router={appRouter} />
+      <Footer/>
     </div>
   );
 }
